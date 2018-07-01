1b10565fa3ea38c16dc51c815f1c7eec56944334
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2018-07-01 11:40:21 +0900

    ブラウザをリサイズ時に、再ロードではなく、カメラの再起動に変更。 右上のボタンによって、カメラを変更する。

--- public/js/barcode.js
+++ public/js/barcode.js
@@ -1,62 +1,95 @@
-const medias = {
-  audio : false,
-  video : {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    frameRate: 10
+class Camera {
+  constructor(){
+    this.video  = document.getElementById("video");
+    this.medias = {
+      audio : false,
+      video : {
+        frameRate: 10
+      }
+    };
   }
-};
-const video  = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const ctx    = canvas.getContext("2d");
+  init(cameraID){
+    this.medias.video.width = window.innerWidth;
+    this.medias.video.height = window.innerHeight;
+    this.medias.video.deviceId = {exact: cameraID};
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
-
-navigator.getUserMedia(medias,(stream)=>{
-  video.srcObject = stream;
-  },(err)=>{
-    window.alert('Error Occurred.');
-    console.log(err);
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
+  
+    navigator.getUserMedia(this.medias,(stream)=>{
+      this.video.srcObject = stream;
+    },(err)=>{
+      window.alert('Error Occurred.');
+      console.error(err);
+    });
+    requestAnimationFrame(canvasDraw);
   }
-);
-requestAnimationFrame(draw);
-function draw() {
+  end(callback){
+    this.video.srcObject = null;
+    if(callback != undefined){
+      callback();
+    }
+  }
+}
+
+function canvasDraw(){
+  const video  = document.getElementById("video");
+  const canvas = document.getElementById("canvas");
+  const ctx    = canvas.getContext("2d");
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
   ctx.drawImage(video, 0, 0, window.innerWidth, window.innerHeight);
-  requestAnimationFrame(draw);
+  requestAnimationFrame(canvasDraw);
 }
 
-function getVideoSources(callback) {
-  if (!navigator.mediaDevices) {
-    console.log("MediaStreamTrack");
-    MediaStreamTrack.getSources((cams)=>{
-      cams.forEach((c, i, a)=>{
-        if (c.kind != 'video') return;
-        callback({
-          name: c.facing,
-          id: c.id
-        });
+function getVideoSources() {
+  return new Promise((resolve, reject)=>{
+    caminfos = [];
+    if (!navigator.mediaDevices) {
+      MediaStreamTrack.getSources((cams)=>{
+        for(let i = 0; i <= cams.length; i++){
+          if(i == cams.length ) resolve(caminfos);
+          else if(cams[i].kind != 'videoinput') continue;
+          else caminfos.push({name:cams[i].facing, id:cams[i].id});
+        }
       });
-    });
-  } else {
-    navigator.mediaDevices.enumerateDevices().then((cams)=>{
-      cams.forEach((c, i, a)=>{
-        console.log("mediaDevices", c);
-        if (c.kind != 'videoinput') return;
-        callback({
-          name: c.label,
-          id: c.deviceId
-        });
+    } else {
+      navigator.mediaDevices.enumerateDevices().then((cams)=>{
+        for(let i = 0; i <= cams.length; i++){
+          if(i == cams.length ) resolve(caminfos);
+          else if(cams[i].kind != 'videoinput') continue;
+          else caminfos.push({name:cams[i].label, id:cams[i].deviceId});
+        }
       });
-    });
-  }
+    }
+  });
+  
 }
 
 {
-  let timer = 0;
-  window.onresize = ()=>{
-    if(timer > 0) clearTimeout(timer);
-    timer = setTimeout(location.reload(), 200);
-  };
+  getVideoSources().then((cams)=>{
+    let selectCameraIndex = 0;
+    const camera = new Camera();
+    camera.init(cams[selectCameraIndex].id);
+    
+    let timer = 0;
+    window.onresize = ()=>{
+      if(timer > 0) clearTimeout(timer);
+      timer = setTimeout(()=>{
+        camera.end(()=>{
+          camera.init();
+        })
+      }, 200);
+    };
+  
+    let chcycle = document.getElementById('chcycle');
+    chcycle.onclick = ()=>{
+      selectCameraIndex++;
+      if(selectCameraIndex == cams.length){
+        selectCameraIndex = 0;
+      }
+      camera.end(()=>{
+        camera.init(cams[selectCameraIndex].id);
+      })
+    };
+  });
 }
